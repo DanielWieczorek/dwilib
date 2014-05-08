c79996d134a18f9870126a659047321bd43be9c5
Daniel Wieczorek <daniel.wieczorek0@gmail.com>
2014-05-08 21:56:03 +0200

    cleaned up some imports

--- src/dwilib.h
+++ src/dwilib.h
@@ -4,19 +4,16 @@
  */
 
 
-#include "dwistring.h"
-#include "dwiio.h"
+
 #include <sys/types.h>
-#include <stdio.h>
 #include <sys/stat.h>
 #include <stdlib.h>
-#include "dwistring.h"
 #include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
-#include <stdlib.h>
+#include "dwistring.h"
+#include "dwiio.h"
 
 #ifndef DWILIB_H_
 #define DWILIB_H_
