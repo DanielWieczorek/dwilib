5d266dbbe0cfe9e34d8d13d0ab40ced1bc907dd9
Daniel Wieczorek <daniel.wieczorek0@gmail.com>
2014-05-31 15:40:13 +0200

    added dwiint + tests and readded the testdata

---
+++ src/dwiint.c
@@ -0,0 +1,12 @@
+
+
+#include "dwiint.h"
+#include "dwiio.h"
+
+uint32_t getRandomUint32(void){
+    uint32_t result = 0;
+    
+    result = (uint32_t)readBytesFromFile("/dev/random",sizeof(uint32_t));
+    return result;
+}
+
---
+++ test/dwiint_test.c
@@ -0,0 +1,53 @@
+/*
+ * File:   dwiint_test.c
+ * Author: Rhox
+ *
+ * Created on 31.05.2014, 14:26:26
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../src/dwilib.h"
+#include <CUnit/Basic.h>
+
+/*
+ * CUnit Test Suite
+ */
+
+int init_suite(void) {
+    return 0;
+}
+
+int clean_suite(void) {
+    return 0;
+}
+
+void test_getRandomUint32() {
+    CU_ASSERT_NOT_EQUAL(getRandomUint32(),getRandomUint32());
+}
+int main() {
+    CU_pSuite pSuite = NULL;
+
+    /* Initialize the CUnit test registry */
+    if (CUE_SUCCESS != CU_initialize_registry())
+        return CU_get_error();
+
+    /* Add a suite to the registry */
+    pSuite = CU_add_suite("dwiint_test", init_suite, clean_suite);
+    if (NULL == pSuite) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    /* Add the tests to the suite */
+    if ((NULL == CU_add_test(pSuite, "test_getRandomUint32", test_getRandomUint32))) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    /* Run all tests using the CUnit Basic interface */
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+    CU_cleanup_registry();
+    return CU_get_error();
+}
