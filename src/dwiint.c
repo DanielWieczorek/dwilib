fc90c3b6250198ae52680dc8dd7ee9e6bee21990
Daniel Wieczorek <daniel.wieczorek0@gmail.com>
2014-06-01 17:08:14 +0200

    added comment for getRandomUint32

--- src/dwiint.c
+++ src/dwiint.c
@@ -2,6 +2,11 @@
 
 #include "dwiint.h"
 
+/**
+ * @brief reads 32 bit from /dev/random and converts them to an int
+ *
+ * @return returns the int that was read from /dev/random
+ */
 uint32_t getRandomUint32(void){
     uint32_t result = 0;
     
